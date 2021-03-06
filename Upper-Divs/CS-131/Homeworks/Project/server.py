08c27c97907642503d1e94b23000f84e7c014660
Charles Zhang <charleszhangmb@gmail.com>
2021-03-06 12:36:27 -0800

    03/06/21 update | CS 131: Project source code update

--- Upper-Divs/CS-131/Homeworks/Project/server.py
+++ Upper-Divs/CS-131/Homeworks/Project/server.py
@@ -244,7 +244,7 @@ def main():
         sys.exit()
     else:
         # Init logs
-        logging.basicConfig(filename=f'./{args.server_name}_log.txt', level=logging.INFO)
+        logging.basicConfig(filename=f'./Logs/{args.server_name}_log.txt', level=logging.INFO)
         # Init server using name, corresponding port, and local IP
         s = Server(args.server_name, server_ports[args.server_name], '127.0.0.1')
         try:
