47f27946c4fee1959b8f34cba2b35e7ac0d44711
Charles Zhang <charleszhangmb@gmail.com>
2021-04-02 19:52:12 -0700

    04/02/21 update | CS 111: Lab 0 update

--- Upper-Divs/CS-111/Labs/Lab-0/proc_count.c
+++ Upper-Divs/CS-111/Labs/Lab-0/proc_count.c
@@ -1,20 +1,35 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
-static int __init proc_count_init(void)
-{
-	pr_info("proc_count: init\n");
+// Global variable
+static struct proc_dir_entry *ent;
+
+// Command that gets run when cat is used
+static int proc_count_show(struct seq_file *m, void *v) {
+	seq_printf(m, "Hello\n");
 	return 0;
 }
 
-static void __exit proc_count_exit(void)
-{
-	pr_info("proc_count: exit\n");
+// Create /proc/count
+static int __init proc_count_init(void) {
+	pr_info("proc_count: init\n");
+	ent = proc_create_single("count", 0, NULL, proc_count_show);
+	return 0;
 }
 
-module_init(proc_count_init);
+// Clean up
+static void __exit proc_count_exit(void) {
+	pr_info("proc_count: exit\n");
+	proc_remove(ent);
+}
+
+// isnmod and rmmod
+module_init(proc_count_init); 
 module_exit(proc_count_exit);
 
+// Lol idk
 MODULE_AUTHOR("Charles Zhang");
 MODULE_DESCRIPTION("Module that shows the current number of processes running");
 MODULE_LICENSE("GPL");
