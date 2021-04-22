26c86e3d1fcc3d2d1b1caec8d903d87952652cba
Charles Zhang <charleszhangmb@gmail.com>
2021-04-21 22:35:19 -0700

    04/21/21 update | CS 111: Lab 1 program

--- Upper-Divs/CS-111/Labs/Lab-1/pipe.c
+++ Upper-Divs/CS-111/Labs/Lab-1/pipe.c
@@ -6,128 +6,109 @@
 
 int main(int argc, char *argv[])
 {
-	// Num args error
+	// Num args error handling
 	if (argc == 1)
 	{
 		perror("No command line arguments found\n");
 		exit(EINVAL);
 	}
-	// Initialize variables + pipe
-	int i, pid, status, input = 0;
-	for (i = 1; i < argc; i++)
+	// Init vars
+	int i, pid = 1, argnum, status;
+	// Init pid container
+	int pids[argc - 1];
+	// Init pipes container
+	int pipes[2 * (argc - 2)];
+	// Init pipes
+	for (int i = 0; i < 2 * (argc - 2); i += 2)
 	{
-		int fds[2]; // [0] = read, [1] = write
-		status = pipe(fds);
-		// Pipe error
+		status = pipe(pipes + i);
+		// pipe error handling
 		if (status == -1)
 		{
 			status = errno;
 			perror("Pipe failed\n");
 			exit(status);
 		}
+	}
+	// Create all children
+	for (i = 1; i < argc; i++)
+	{
 		pid = fork();
-		// Child process
-		if (pid == 0)
+		// Save pid for cleanup later
+		pids[i - 1] = pid;
+		// Save argnum for indexing
+		argnum = i;
+		// fork error handling
+		if (pid < 0)
 		{
-			status = dup2(input, STDIN_FILENO);
-			// dup2 error
+			status = errno;
+			perror("Fork failed\n");
+			exit(status);
+		}
+		// If child, stop forking
+		else if (pid == 0)
+			break;
+	}
+	// Child processes
+	if (pid == 0)
+	{
+		// Connect read end of last pipe to fd = 0 of current process
+		if (argnum != 1)
+		{
+			status = dup2(pipes[2 * (argnum - 2)], STDIN_FILENO);
+			// dup2 error handling
 			if (status == -1)
 			{
 				status = errno;
 				perror("Failed to link to previous pipe\n");
 				exit(status);
 			}
-			if (i < argc - 1)
-			{
-				status = dup2(fds[1], STDOUT_FILENO);
-				// dup2 error
-				if (status == -1)
-				{
-					status = errno;
-					perror("Failed to link to write end of pipe\n");
-					exit(status);
-				}
-			}
-			status = close(fds[0]);
-			if (status == -1)
-			{
-				status = errno;
-				perror("Failed to close\n");
-				exit(status);
-			}
-			status = close(fds[1]);
-			if (status == -1)
-			{
-				status = errno;
-				perror("Failed to close\n");
-				exit(status);
-			}
-			status = execlp(argv[i], argv[i], NULL);
-			// execlp error
-			if (status == -1)
-			{
-				status = errno;
-				perror("execlp failed\n");
-				exit(status);
-			}
 		}
-		// Parent process
-		else if (pid > 0)
+		// Connect write end of current pipe to fd = 1 of current process
+		if (argnum != argc - 1)
 		{
-			status = 0;
-			waitpid(pid, &status, 0);
-			close(fds[1]);
+			status = dup2(pipes[2 * argnum - 1], STDOUT_FILENO);
+			// dup2 error handling
 			if (status == -1)
 			{
 				status = errno;
-				perror("Failed to close\n");
+				perror("Failed to link to previous pipe\n");
 				exit(status);
 			}
-			input = fds[0];
 		}
-		// Fork error
-		else
+		// Close file descriptors
+		for (i = 0; i < 2 * (argc - 2); i++)
+			close(pipes[i]);
+		// Use execlp to change processes
+		status = execlp(argv[argnum], argv[argnum], NULL);
+		// execlp error handling
+		if (status == -1)
 		{
 			status = errno;
-			perror("Fork failed\n");
+			perror("execlp failed\n");
 			exit(status);
+		}
+	}
+	// Parent process
+	else
+	{
+		// Close file descriptors
+		for (i = 0; i < 2 * (argc - 2); i++)
+			close(pipes[i]);
+		for (i = 0; i < argc - 1; i++)
+		{
+			// Wait for child processes
+			if (waitpid(pids[i], &status, 0) == -1)
+			{
+				status = errno;
+				perror("waitpid failed\n");
+				exit(errno);
+			}
+			// Check for error codes from children
+			if (WIFEXITED(status))
+				if (WEXITSTATUS(status) != 0)
+					exit(WEXITSTATUS(status));
 		}
 	}
 	return 0;
 }
-
-/* ------------------------------------------------------------------------------------
-Simple pipe: the child takes the first arg by closing stdout/the read end, then pipes it
-to the parent, which takes the second arg by closing stdin/the write end
------------------------------------------------------------------------------------- */
-
-// int fds[2]; // [0] is read end, [1] is write end
-// pipe(fds);
-// int ret = fork();
-// // Child process
-// if (ret == 0)
-// {
-// 	close(STDOUT_FILENO);
-// 	// Child isn't going to use the read pipe
-// 	close(fds[0]);
-// 	// dup the write end of the pipe as stdout
-// 	dup(fds[1]);
-// 	// Become the first process
-// 	execlp(argv[1], argv[1], NULL);
-// 	// Close on error
-// 	close(fds[1]);
-// }
-// // Parent process
-// else if (ret > 0)
-// {
-// 	close(STDIN_FILENO);
-// 	// Parent isn't going to use the write pipe
-// 	close(fds[1]);
-// 	// dup the read end of the pipe as stdin
-// 	dup(fds[0]);
-// 	// Become the second process
-// 	execlp(argv[2], argv[2], NULL);
-// 	// Close on error
-// 	close(fds[0]);
-// }
-// return 0;
