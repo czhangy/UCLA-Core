145c032c0e60e85f9517e73e7a12cd44420fae98
Charles Zhang <charleszhangmb@gmail.com>
2021-11-03 11:12:57 -0700

    11/03/21 update | CS M152A: Lab 3 definitions

--- Upper-Divs/CS-M152A/Labs/Lab-3/Code/stopwatch_definitions.v
+++ Upper-Divs/CS-M152A/Labs/Lab-3/Code/stopwatch_definitions.v
@@ -1,14 +1,14 @@
 // Cathode values
-parameter ST_0000 = 7'b0000001;
-parameter ST_0001 = 7'b1001111;
-parameter ST_0010 = 7'b0010010;
-parameter ST_0011 = 7'b0000110;
-parameter ST_0100 = 7'b1001100;
-parameter ST_0101 = 7'b0100100;
-parameter ST_0110 = 7'b0100000;
-parameter ST_0111 = 7'b0001111;
+parameter ST_0000 = 7'b1000000;
+parameter ST_0001 = 7'b1111001;
+parameter ST_0010 = 7'b0100100;
+parameter ST_0011 = 7'b0110000;
+parameter ST_0100 = 7'b0011001;
+parameter ST_0101 = 7'b0010010;
+parameter ST_0110 = 7'b0000010;
+parameter ST_0111 = 7'b1111000;
 parameter ST_1000 = 7'b0000000;
-parameter ST_1001 = 7'b0000100;
+parameter ST_1001 = 7'b0010000;
 parameter ST_OFF = 7'b1111111;
 
 // Anode values
