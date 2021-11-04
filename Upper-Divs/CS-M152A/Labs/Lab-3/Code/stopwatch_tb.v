80c5cf274baf40ec98c3cb51624e8eb9c6635b5d
Charles Zhang <charleszhangmb@gmail.com>
2021-11-03 19:06:19 -0700

    11/03/21 update | CS M152A: Lab 3 testbench

--- Upper-Divs/CS-M152A/Labs/Lab-3/Code/stopwatch_tb.v
+++ Upper-Divs/CS-M152A/Labs/Lab-3/Code/stopwatch_tb.v
@@ -72,28 +72,64 @@ module stopwatch_tb;
 		// Test one second default
 		#500000000;
 		
-		$display("Normal mode test complete");
+		$display("-------------------------------------------");
+		$display("Normal mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 		
 		// Test ADJ, SEL = 0
 		ADJ = 1;
 		#1100000;
 		#500000000;
 		
-		$display("Minute adjustment mode test complete");
+		$display("-------------------------------------------");
+		$display("Minute adjustment mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 		
 		// Test ADJ, SEL = 1
 		SEL = 1;
 		#1100000;
 		#500000000;
 		
-		$display("Second adjustment mode test complete");
+		$display("-------------------------------------------");
+		$display("Second adjustment mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 		
 		// Test ADJ = 0
 		ADJ = 0;
 		#1100000;
 		#1000000000;
 		
-		$display("ADJ off mode test complete");
+		$display("-------------------------------------------");
+		$display("ADJ off mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 		
 		// Test PAUSE
 		PAUSE = 1;
@@ -102,7 +138,16 @@ module stopwatch_tb;
 		PAUSE = 0;
 		#1100000;
 		
-		$display("Pause test complete");
+		$display("-------------------------------------------");
+		$display("Pause test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 		
 		// Test Unpause
 		PAUSE = 1;
@@ -111,11 +156,31 @@ module stopwatch_tb;
 		PAUSE = 0;
 		#1100000;
 		
+		$display("-------------------------------------------");
+		$display("Unpause test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
 		// Test RESET
 		RESET = 1;
 		#1100000;
 		
-		$display("Reset test complete");
+		$display("-------------------------------------------");
+		$display("Reset test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
 
 	end
       
