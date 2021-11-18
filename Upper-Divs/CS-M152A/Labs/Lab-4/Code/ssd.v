94e5a7434efed39d8bceec79d821813e64382252
Charles Zhang <charleszhangmb@gmail.com>
2021-11-17 19:09:57 -0800

    11/17/21 update | CS M152A: Lab 4 SSD

--- Upper-Divs/CS-M152A/Labs/Lab-4/Code/ssd.v
+++ Upper-Divs/CS-M152A/Labs/Lab-4/Code/ssd.v
@@ -5,7 +5,7 @@
 // 
 // Create Date:    01:50:16 10/31/2021 
 // Design Name: 
-// Module Name:    create_display 
+// Module Name:    ssd 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,11 +18,11 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module create_display(
+module ssd(
 	// Inputs
 	input clk_fst,
-	input [6:0] p1_cathode,
-	input [6:0] p2_cathode,
+	input [3:0] p1_state,
+	input [3:0] p2_state,
 	// Outputs
 	output reg [6:0] cathode,
 	output reg [3:0] anode
@@ -31,23 +31,32 @@ module create_display(
 	// Get constants
 	`include "battleship_definitions.v"
 
-	// Specify which digit
+	// Cathode values
+	wire [6:0] p1_cathode;
+	wire [6:0] p2_cathode;
+
+	// Digit counter
 	reg [1:0] digit = 0;
 	
+	// Translate states to cathodes
+	get_cathode p1 (.display_state(p1_state), .cathode(p1_cathode));
+	get_cathode p2 (.display_state(p2_state), .cathode(p2_cathode));
+
+	// Display on SSD
 	always @(posedge clk_fst) begin
 		// Handle P1's left digit
 		if (digit == 0) begin
-			digit <= digit + 1'b1;
+			digit <= digit + 1;
 			cathode <= ST_OFF;
 			anode <= P1_LEFT;
 		// Handle P1's right digit
 		end else if (digit == 1) begin
-			digit <= digit + 1'b1;
+			digit <= digit + 1;
 			cathode <= p1_cathode;
 			anode <= P1_RIGHT;
 		// Handle P2's left digit
 		end else if (digit == 2) begin
-			digit <= digit + 1'b1;
+			digit <= digit + 1;
 			cathode <= ST_OFF;
 			anode <= P2_LEFT;
 		// Handle P2's right digit
