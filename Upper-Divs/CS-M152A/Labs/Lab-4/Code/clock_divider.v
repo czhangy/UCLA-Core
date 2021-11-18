37d6f32cbde7f1c4f6b5d775bf63bf78c918ae64
Charles Zhang <charleszhangmb@gmail.com>
2021-11-17 18:58:42 -0800

    11/17/21 update | CS M152A: Lab 4 clock divider

---
+++ Upper-Divs/CS-M152A/Labs/Lab-4/Code/clock_divider.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    08:43:55 10/27/2021 
+// Design Name: 
+// Module Name:    clock_divider 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clock_divider(
+	// Input
+	input clk, // 100MHz
+	// Output
+	output reg clk_ssd, // 400,000 clock cycles (250 Hz)
+	output reg clk_vga // 4 clock cycles (25 MHz)
+);
+
+	// Get constants
+	`include "battleship_definitions.v"
+	
+	// Counter
+	reg [31:0] ssd_counter = 0; // counts to 200,000
+	reg [31:0] vga_counter = 0; // counts to 2
+	
+	always @(posedge clk) begin
+		// Increment counters
+		ssd_counter <= ssd_counter + 1;
+		vga_counter <= vga_counter + 1;
+		// Check counters
+		if (ssd_counter >= CLK_SSD_COUNT) begin
+			ssd_counter <= 0;
+			clk_ssd <= ~clk_ssd;
+		end
+		if (vga_counter >= CLK_VGA_COUNT) begin
+			vga_counter <= 0;
+			clk_vga <= ~clk_vga;
+		end
+	end
+
+endmodule
