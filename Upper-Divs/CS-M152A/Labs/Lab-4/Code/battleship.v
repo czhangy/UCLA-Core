12ce8652d04b41d23d800e55ffb2ae5f8401fd09
Charles Zhang <charleszhangmb@gmail.com>
2021-11-18 19:25:49 -0800

    11/18/21 update | CS M152A: Lab 4 top module

--- Upper-Divs/CS-M152A/Labs/Lab-4/Code/battleship.v
+++ Upper-Divs/CS-M152A/Labs/Lab-4/Code/battleship.v
@@ -56,38 +56,57 @@ module battleship(
 	// Cathode values
 	wire [6:0] p1_cathode;
 	wire [6:0] p2_cathode;
+
+	// Score
+	wire [2:0] p1_ships;
+	wire [2:0] p2_ships;
+	assign p1_ships = 5;
+	assign p2_ships = 5;
 	
 	// Game boards
 
 	// Debounce all inputs
-	debouncer db_rst (.clk(clk), .signal_i(RST), .signal_f(rst));
-	debouncer db_btnC (.clk(clk), .signal_i(BTNC), .signal_f(btnC));
-	debouncer db_btnL (.clk(clk), .signal_i(BTNL), .signal_f(btnL));
-	debouncer db_btnR (.clk(clk), .signal_i(BTNR), .signal_f(btnR));
-	debouncer db_btnU (.clk(clk), .signal_i(BTNU), .signal_f(btnU));
-	debouncer db_btnD (.clk(clk), .signal_i(BTND), .signal_f(btnD));
-	debouncer db_sw0 (.clk(clk), .signal_i(SW0), .signal_f(sw0));
-	debouncer db_sw1 (.clk(clk), .signal_i(SW1), .signal_f(sw1));
-	debouncer db_sw2 (.clk(clk), .signal_i(SW2), .signal_f(sw2));
-	debouncer db_sw3 (.clk(clk), .signal_i(SW3), .signal_f(sw3));
-	debouncer db_sw4 (.clk(clk), .signal_i(SW4), .signal_f(sw4));
-	debouncer db_sw5 (.clk(clk), .signal_i(SW5), .signal_f(sw5));
-	debouncer db_sw6 (.clk(clk), .signal_i(SW6), .signal_f(sw6));
-	debouncer db_sw7 (.clk(clk), .signal_i(SW7), .signal_f(sw7));
+	debouncer db_rst(.clk(clk), .signal_i(RST), .signal_f(rst));
+	debouncer db_btnC(.clk(clk), .signal_i(BTNC), .signal_f(btnC));
+	debouncer db_btnL(.clk(clk), .signal_i(BTNL), .signal_f(btnL));
+	debouncer db_btnR(.clk(clk), .signal_i(BTNR), .signal_f(btnR));
+	debouncer db_btnU(.clk(clk), .signal_i(BTNU), .signal_f(btnU));
+	debouncer db_btnD(.clk(clk), .signal_i(BTND), .signal_f(btnD));
+	debouncer db_sw0(.clk(clk), .signal_i(SW0), .signal_f(sw0));
+	debouncer db_sw1(.clk(clk), .signal_i(SW1), .signal_f(sw1));
+	debouncer db_sw2(.clk(clk), .signal_i(SW2), .signal_f(sw2));
+	debouncer db_sw3(.clk(clk), .signal_i(SW3), .signal_f(sw3));
+	debouncer db_sw4(.clk(clk), .signal_i(SW4), .signal_f(sw4));
+	debouncer db_sw5(.clk(clk), .signal_i(SW5), .signal_f(sw5));
+	debouncer db_sw6(.clk(clk), .signal_i(SW6), .signal_f(sw6));
+	debouncer db_sw7(.clk(clk), .signal_i(SW7), .signal_f(sw7));
 	
 	// Divide the master clock signal
-	clock_divider clk_div (.clk(clk), .rst(btnC), .clk_vga(clk_vga), .clk_ssd(clk_ssd));
+	clock_divider clk_div(
+		.clk(clk),
+		.rst(btnC),
+		.clk_ssd(clk_ssd),
+		.clk_vga(clk_vga)
+	);
 	
-	assign cathode = 7'b0000000;
-	assign anode = 4'b1011;
+	// Handle SSD
+	ssd ssd_display(
+		.clk_ssd(clk_ssd),
+		.p1_ships(p1_ships),
+		.p2_ships(p2_ships),
+		.cathode(cathode),
+		.anode(anode)
+	);
 	
 	// Handle VGA
-	vga vga_display(.clk_vga(clk_vga),
-					.rst(btnC),
-					.hsync(hsync),
-					.vsync(vsync),
-					.red(red),
-					.green(green),
-					.blue(blue));
+	vga vga_display(
+		.clk_vga(clk_vga),
+		.rst(btnC),
+		.hsync(hsync),
+		.vsync(vsync),
+		.red(red),
+		.green(green),
+		.blue(blue)
+	);
 
 endmodule
